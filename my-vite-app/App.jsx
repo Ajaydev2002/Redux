61e0d9fd686c1076af19a04afca079f58f737683
ajaydev2002 <ajay.p@rigelsoft.in>
2025-04-22 15:16:19 +0530

    updated code

--- my-vite-app/App.jsx
+++ my-vite-app/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import Main from "./my-vite-app/src/components/Main";
-import store from "./my-vite-app/src/redux/store";
-import AddDetails from "./my-vite-app/src/components/AddDetails";
-import Form from "./my-vite-app/src/components/UseReducer";
-import TaskApp from "./my-vite-app/src/components/TodoList";
+import Main from "../my-vite-app/src/components/Main";
+import store from "../my-vite-app/src/redux/store";
+import AddDetails from "../my-vite-app/src/components/AddDetails";
+import Form from "../my-vite-app/src/components/UseReducer";
+import TaskApp from "../my-vite-app/src/components/TodoList";
 
 const Applayout = () => {
   return (
